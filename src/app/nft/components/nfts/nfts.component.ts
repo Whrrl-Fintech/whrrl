f88fa8f6b554c7a6e32a858c98a7f396f6d61afa
whrrlchain <harshala@whr.loans>
2020-10-11 00:57:28 +0530

    first commit

---
+++ src/app/nft/components/nfts/nfts.component.ts
@@ -0,0 +1,301 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NftService } from '../../services/nft.service';
+import { MatSnackBar } from '@angular/material';
+import { Web3Service } from '../../../common/services/web3.service';
+
+// import $ from "jquery";
+declare var $: any;
+
+@Component({
+  selector: 'app-nfts',
+  templateUrl: './nfts.component.html',
+  styleUrls: ['./nfts.component.css']
+})
+export class NftsComponent implements OnInit {
+  whr: any;
+  balanceOf: any;
+  totalCal: any;
+  totalInt: any;
+  paused: any;
+  repaymentAmount: any;
+  loanAmount: any;
+  feeDepositor: any;
+  lenderAddress: any;
+  // web3Service: any;
+  userAddress: any;
+
+  constructor(private nftService: NftService,
+    private snackBar: MatSnackBar, private web3Service: Web3Service) {
+  }
+  ngOnInit() {
+    this.user()
+  }
+  user() {
+    this.userAddress = localStorage.getItem("sc");
+    console.log("################## NFT #####", this.userAddress);
+  }
+  data: any = [
+    {
+      "id": 0,
+      "commodity": "Wheat",
+      "picture": "../../../../assets/images/Wheat.jpg",
+      "totalQtl": 3,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 1,
+      "commodity": "Oats",
+      "picture": "../../../../assets/images/Oats.jpg",
+      "totalQtl": 3,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 2,
+      "commodity": "Corn",
+      "picture": "../../../../assets/images/Corn.jpg",
+      "totalQtl": 2,
+      "variety": "Corn 2",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 3,
+      "commodity": "Barley",
+      "picture": "../../../../assets/images/Barley.jpg",
+      "totalQtl": 2,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 4,
+      "commodity": "Sorghum",
+      "picture": "../../../../assets/images/Sorghum.jpg",
+      "totalQtl": 2,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 5,
+      "commodity": "Rye",
+      "picture": "../../../../assets/images/Rye.jpg",
+      "totalQtl": 3,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 6,
+      "commodity": "Amaranth",
+      "picture": "../../../../assets/images/Amaranth.jpg",
+      "totalQtl": 3,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 7,
+      "commodity": "Millet",
+      "picture": "../../../../assets/images/Millet.jpg",
+      "totalQtl": 3,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 8,
+      "commodity": "Triticale",
+      "picture": "../../../../assets/images/Triticale.jpg",
+      "totalQtl": 2,
+      "variety": "Normal",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 9,
+      "commodity": "Buckwheat",
+      "picture": "../../../../assets/images/Buckwheat.jpg",
+      "totalQtl": 3,
+      "variety": "Buckwheat",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 10,
+      "commodity": "Quinoa",
+      "picture": "../../../../assets/images/Quinoa.jpg",
+      "totalQtl": 2,
+      "variety": "Quinoa",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    },
+    {
+      "id": 11,
+      "commodity": "Teff",
+      "picture": "../../../../assets/images/Teff.jpg",
+      "totalQtl": 3,
+      "variety": "Teff",
+      "location": "Maharastra, India",
+      "sc": "0xca48bfea13505472bacd1d6fdf237a28de939b4d"
+    }
+
+  ]
+
+  whrDetails(h) {
+
+    this.nftService.getWhrDetails().then((data) => {
+      this.whr = data;
+      this.whr['picture'] = h.picture;
+      this.whr['commodity'] = h.commodity;
+      console.log(data);
+      // this.balanceOf=balanceOf;
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+    this.nftService.balanceOf().then((data) => {
+      // this.whr=data;
+      console.log(data);
+      this.balanceOf = data;
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+
+    this.nftService.totalCal().then((data) => {
+      // this.whr=data; 
+      console.log(data);
+
+      this.totalCal = data / Math.pow(10, 18);
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+
+    this.nftService.totalInt().then((data) => {
+      // this.whr=data;
+      this.totalInt = data / Math.pow(10, 18);
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+    this.nftService.paused().then((data) => {
+      // this.whr=data;
+      this.paused = data;
+      console.log('paused', data);
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+    this.nftService.lenderAddress().then((data) => {
+      // this.whr=data;
+      this.lenderAddress = data;
+      console.log('feeDepositor', this.lenderAddress);
+      // this.feeDepositor = data;
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+    this.nftService.repaymentAmount().then((data) => {
+      // this.whr=data;
+      this.repaymentAmount = data / Math.pow(10, 18);
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+    this.nftService.loanAmount().then((data) => {
+      // this.whr=data;
+      this.loanAmount = data / Math.pow(10, 18);
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+
+
+    this.userAddress = this.nftService.userAddress();
+    console.log(this.userAddress);
+
+  }
+
+  showWhrDetails(h) {
+    console.log(h);
+    localStorage.setItem("sc", h.sc)
+    this.whrDetails(h);
+    // window.location.href = '/sc';
+    $('#msg').modal('show');
+  }
+
+
+  borrowerWithdraw() {
+    this.nftService.borrowerWithdraw().then((data) => {
+      // this.whr=data;
+      this.paused = data;
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+  }
+
+  lenderWithdraw() {
+    this.nftService.lenderWithdraw().then((data) => {
+      // this.whr=data;
+      this.paused = data;
+      // this.snackBar.open('You review has been sent', '', {
+      //   duration: 2000,
+      // });
+    });
+  }
+
+
+  sentEth() {
+    let sc = localStorage.getItem("sc")
+    console.log(sc, this.userAddress, '1');
+
+    this.nftService.transfer(sc, this.userAddress
+      , this.whr.ethprice/(70/100)).then((data) => {
+        // this.whr=data;
+        this.paused = data;
+        // this.snackBar.open('You review has been sent', '', {
+        //   duration: 2000,
+        // });
+      });
+  }
+
+
+  repaymentEth() {
+    let sc = localStorage.getItem("sc")
+    console.log(sc, this.userAddress, '1');
+    this.nftService.transfer(sc, this.userAddress
+      , this.whr.ethprice/(70/100)).then((data) => {
+        // this.whr=data;
+        this.paused = data;
+        // this.snackBar.open('You review has been sent', '', {
+        //   duration: 2000,
+        // });
+      });
+  }
+
+  mint() {
+    let sc = localStorage.getItem("sc")
+    console.log(sc, this.userAddress, '1');
+    this.nftService.transfer(sc, this.userAddress
+      , this.whr.ethprice/(70/100)).then((data) => {
+        // this.whr=data;
+        this.paused = data;
+        // this.snackBar.open('You review has been sent', '', {
+        //   duration: 2000,
+        // });
+      });
+  }
+}
---
+++ src/app/nft/services/nft.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { Web3Service } from '../../common/services/web3.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NftService {
+
+  constructor(private web3Service: Web3Service) {
+  }
+
+  getWhrDetails(): Promise<any> {
+    return this.web3Service.abi.methods.whr().call();
+  }
+
+  balanceOf(): Promise<any> {
+    return this.web3Service.abi.methods.balanceOf(this.web3Service.getAccount()).call();
+  }
+
+  baseURL(): Promise<any> {
+    return this.web3Service.abi.methods.baseURL().call();
+  }
+
+  feeDepositor(): Promise<any> {
+    return this.web3Service.abi.methods.feeDepositor().call();
+  }
+
+  
+
+  lenderAddress(): Promise<any> {
+    return this.web3Service.abi.methods.lenderAddress().call();
+  }
+
+  loanAmount(): Promise<any> {
+    return this.web3Service.abi.methods.loanAmount().call();
+  }
+
+  loanAmtToPay(): Promise<any> {
+    return this.web3Service.abi.methods.loanAmtToPay().call();
+  }
+
+  name(): Promise<any> {
+    return this.web3Service.abi.methods.name().call();
+  }
+
+
+  owner(): Promise<any> {
+    return this.web3Service.abi.methods.owner().call();
+  }
+
+  paused(): Promise<any> {
+    return this.web3Service.abi.methods.paused().call();
+  }
+
+
+  repaymentAmount(): Promise<any> {
+    return this.web3Service.abi.methods.repaymentAmount().call();
+  }
+
+  totalInt(): Promise<any> {
+    return this.web3Service.abi.methods.totalInt().call();
+  }
+
+  totalCal(): Promise<any> {
+    return this.web3Service.abi.methods.totalCal().call();
+  }
+
+  borrowerWithdraw(): Promise<any> {
+    return this.web3Service.abi.methods.borrowerWithdraw().send({from: this.web3Service.getAccount()});
+  }
+
+  lenderWithdraw(): Promise<any> {
+    console.log('getAccount', this.web3Service.getAccount());
+    return this.web3Service.abi.methods.lenderWithdraw().send({from: this.web3Service.getAccount()});
+  }
+
+  userAddress(): String{
+    console.log('getAccount', this.web3Service.getAccount());
+    return   this.web3Service.getAccount();
+  }
+
+
+  transfer(to,from,value ): Promise<any> {
+    return this.web3Service.trasfer(to,from,value ).send({from: this.web3Service.getAccount()});
+  }
+
+  mint(to,from,value ): Promise<any> {
+    return this.web3Service.trasfer(to,from,value ).send({from: this.web3Service.getAccount()});
+  }
+}
